505a99f07e506e6308247226b1b4908c47338dd5
nicole <nikkihmltn1@yahoo.com>
2022-02-03 00:22:28 -0800

    moved everything around and finally got backend and frontend talking

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const dbUrl = process.env.MONGO_URL;
+
+mongoose
+	.connect(dbUrl)
+	.then(() =>
+		console.log(
+			`MongoDB successfully connected at ${db.host}:${db.port}! How dope!`
+		)
+	)
+	.catch((err) => console.log(`MongoDB connection FAILED :( Error: ${err}`));
+
+module.exports = {
+	
+};
---
+++ server/server.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+/* ==== External Modules ==== */
+const express = require("express");
+const methodOverride = require("method-override");
+
+/* ==== Internal Modules ==== */
+const routes = require("./routes");
+require("./config/database")
+/* ==== Instanced Modules  ==== */
+const app = express();
+
+/* ==== Configuration ==== */
+const config = require("@myflexspace/config");
+
+/* ====  Middleware  ==== */
+// body data middleware
+app.use(express.urlencoded({ extended: true }));
+// method override middleware
+app.use(methodOverride("_method"));
+// serve public files
+//for deployment
+// app.use(express.static("../client/build"));
+// logger
+app.use((req, res, next) => {
+	console.log(req.url, req.method);
+	next();
+});
+
+/* ====  Routes & Controllers  ==== */
+
+
+
+// Internal Routes
+// All of our routes will start with "/api", we're going to route them through index.js
+// app.use("/api", routes);
+
+//This is to catch anything that's trying to hit an api route that isn't made
+// app.all("/api/*", function (req, res, next) {
+// 	res.send("THIS IS NOT AN API ROUTE");
+// });
+
+//THIS IS THE REACT FULL STACK MAGIC MIDDLEWARE
+// Anything that is NOT an api route will therefore be handled by React Router, which is set up right here. The API routes must hit first, order matters, then react via the React build directory.
+/*
+ANY REQUEST not covered by routes express makes -- will get piped to this middleware
+and this middleware's job is to handover control to react
+*/
+
+app.use((req, res, next) => {
+	console.log(req.headers);
+	res.sendFile("../client/build", "index.html");
+});
+/* ====  Server Listener  ==== */
+app.listen(config.PORT, () => {
+	console.log(`myflexspace is live at http://localhost:${config.PORT}`);
+});
