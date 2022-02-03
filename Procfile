ce34918ecb925ac2ee84c49d8c91ec44ea51fc66
nicole <nikkihmltn1@yahoo.com>
2022-02-02 21:20:12 -0800

    set up procfile

---
+++ Procfile
@@ -0,0 +1,3 @@
+//this is for npm testing purposes for this app.
+//use 'yarn start' instead for yarn created react apps
+web: npm start 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./pages/HomePage";
+import Home from "./containers/HomePage";
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
